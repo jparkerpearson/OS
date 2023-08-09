faa35803f574ebab9f5e68a422f990b3931b9cac
Parker Pearson <johnparkerpearson@gmail.com>
2023-08-08 20:58:18 -0700

    Add a custom printf that prints to the video memory

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,12 @@
-#include <iostream>
+
+void printf(char* str)
+{
+   unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+   for(int i= 0; str[i] != '\0'; ++i) {
+      VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+   }
+}
 
 void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
