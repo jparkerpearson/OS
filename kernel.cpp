0fbe01a482179598408ee7d541f111db990ef6b9
Parker Pearson <johnparkerpearson@gmail.com>
2023-08-10 16:42:13 -0700

    Fix typo

--- kernel.cpp
+++ kernel.cpp
@@ -11,7 +11,7 @@ void printf(char* str)
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
-extern "C" void callConstuctors()
+extern "C" void callConstructors()
 {
    for(constructor* i = &start_ctors; i != &end_ctors; i++) {
       (*i)();
