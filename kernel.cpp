7db162290e42ee639922f016261b7d88f45d6e77
Parker Pearson <johnparkerpearson@gmail.com>
2023-08-10 16:38:16 -0700

    Fix issue with the constructos

--- kernel.cpp
+++ kernel.cpp
@@ -8,6 +8,16 @@ void printf(char* str)
    }
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstuctors()
+{
+   for(constructor* i = &start_ctors; i != &end_ctors; i++) {
+      (*i)();
+   }
+}
+
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
    printf("Hello World! Welcome to my OS.");
