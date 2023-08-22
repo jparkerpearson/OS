6656a9b1cfa1407cefeb946f220e134904d9bfd7
Parker Pearson <johnparkerpearson@gmail.com>
2023-08-22 14:51:25 -0700

    Update print

--- kernel.cpp
+++ kernel.cpp
@@ -5,13 +5,47 @@ void printf(char *str)
 {
    // Whatever is written here will be put on the screen by the graphics card.
    // We want 2 byte pointers because first byte is color information and second byte is the character.
-   uint16_t *VideoMemory = (uint16_t *)0xb8000;
+   static uint16_t *VideoMemory = (uint16_t *)0xb8000;
+
+   static uint8_t x = 0, y = 0;
 
    for (int i = 0; str[i] != '\0'; ++i)
    {
-      // We can use a pointer as an array to access subsequent memomory.
-      // Keep the color information in the first byte, but replace the second byte with the char.
-      VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
+      switch (str[i])
+      {
+      case '\n':
+         x = 0;
+         y++;
+         break;
+      default:
+         // We can use a pointer as an array to access subsequent memomory.
+         // Keep the color information in the first byte, but replace the second byte with the char.
+         VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+         x++;
+      }
+
+      // The screen is 80 characters wide and 25 characters tall.
+      if (x >= 80)
+      {
+         x = 0;
+         y++;
+      }
+
+      // If we reach the end of the scree clear the screen.
+      if (y >= 25)
+      {
+         for (y = 0; y < 25; y++)
+         {
+            for (x = 0; x < 80; x++)
+            {
+               VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+            }
+         }
+
+         x = 0;
+         y = 0;
+      }
    }
 }
 
@@ -28,7 +62,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void *multiboot_structure, uint32_t magicnumber)
 {
-   printf("Hello World! Welcome to my OS.");
+   printf("Hello World! Welcome to my OS. \n");
+   printf("My name is Parker.");
 
    GlobalDescriptorTable gdt;
 
