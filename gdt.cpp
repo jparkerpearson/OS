32ff15b37e095c072d7bf7b85b980b81a9debc4a
Parker Pearson <johnparkerpearson@gmail.com>
2023-08-22 11:32:00 -0700

    Fix up comments as i'm developing a better understanding

--- gdt.cpp
+++ gdt.cpp
@@ -1,15 +1,21 @@
 #include "gdt.h"
 #include "types.h"
 
-// Instructions are of the following format:
+// Instructions are of the following format: See (https://en.wikipedia.org/wiki/Segment_descriptor) for more info.
 
 ////
-//  Base 24:31  | Flags 0:3 | Limit 16:19 | Access / Type 0:7 |    Base 16:23
-//  Byte 7      | Byte 6                  |     Byte 5        |     Byte 4
+//  Base 24:31  | Granularity Bit | Default operand size bit | (unused long bit) | Available bit for software |
+//   Byte 7     |                                           Byte 6
+//      Limit 16:19 | Present Bit | Privelage level (2 bits) | System Segment Bit | Type 0:3 |    Base 16:23
+//                                  Byte 5                                                   |     Byte 4
 //          -----------------------------
 //    Base 0:15     |   Limit 0:15
 //  Byte 3 | Byte 2 | Byte 1 | Byte 0
 //////
+/// Meanings
+//      Granularity Bit - if clear limit in units of bytes, else limit in units of 4kb blocks
+//      Default operand size bit - if clear 16 bit mode, else 32 bit mode. We always set to 32 bit mode.
+//
 
 // The limit is the size of the segment, base is the start of the segment, access is the type of segment it is
 GlobalDescriptorTable::GlobalDescriptorTable()
@@ -54,15 +60,17 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base, uint3
     // If the limit is less then 16 bit limit
     if (limit <= 65536)
     {
-        // Tells the processor this is a 16 bit entry
+        // Tells the processor to set granularity to 0 and operand to 1. This will use bytes for the limit.
         target[6] = 0x40;
     }
 
     else
     {
-        // We want to make sure the limit is in 4kb blocks. If the last 12 bits are all 1's, then we know its in 4kb blocks.
-        // We only have 20 bits of limit to store data in even though we are given a 32 bit number. Therefore we shift by 12 bits.
-        // If its not a 4kb block we shift by 12 and subtract 1.
+        // If it is larger than 16 bit limit then we're gonna use 4kb blocks.
+        // If the limit passed in ends in 12 1's then it is already a multiple
+        // of 4kb blocks so we just use the first 20 bits. If it's not a multiple
+        // we subtract 1 to 'round' down. That way we are not overlapping memomory
+        // that might be used for something else.
         if (limit & 0xFFF != 0xFFF)
         {
             limit = (limit >> 12) - 1;
@@ -72,6 +80,7 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base, uint3
             limit = limit >> 12;
         }
 
+        // Tells the processor to set granularity to 1 and operand to 1. This will use pages for the limit.
         target[6] = 0xC0;
     }
 
@@ -111,6 +120,7 @@ uint32_t GlobalDescriptorTable::SegmentDescriptor::Limit()
     result = (result << 8) + target[1];
     result = (result << 8) + target[0];
 
+    // If the granularity bit is set then we need to multiply by 4kb
     if (target[6] & 0xC0 == 0xC0)
     {
         result = (result << 12) | 0xFFFF;
